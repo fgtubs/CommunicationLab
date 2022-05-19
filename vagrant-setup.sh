66abb056be76b4f82d24432f94c71a2a3b34f6d4
fgtubs <87946264+fgtubs@users.noreply.github.com>
2022-05-19 16:47:12 +0200

    Update vagrant-setup.sh

--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -3,18 +3,18 @@
 # Install Vagrant 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install vagrant
+sudo apt-get update && sudo apt-get install -y vagrant
 
 # Install KVM
-sudo apt install qemu qemu-kvm libvirt-clients libvirt-daemon-system virtinst bridge-utils
+sudo apt install -y qemu qemu-kvm libvirt-clients libvirt-daemon-system virtinst bridge-utils
 kvm-ok
 
 # Install libvirt and vagrant plugins
-sudo apt install qemu libvirt-daemon-system libvirt-clients libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev ruby-libvirt ebtables dnsmasq-base
-sudo apt-get install build-essential
+sudo apt install -y qemu libvirt-daemon-system libvirt-clients libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev ruby-libvirt ebtables dnsmasq-base
+sudo apt-get install -y build-essential
 vagrant plugin install vagrant-libvirt
 vagrant plugin install vagrant-mutate
 
 # Install NFS
 sudo apt update
-sudo apt install nfs-kernel-server
+sudo apt install -y nfs-kernel-server
