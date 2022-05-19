f041328f57f84436bbc8e3fb4b254585db2f32d0
fgtubs <87946264+fgtubs@users.noreply.github.com>
2022-05-19 11:32:02 +0200

    Add files via upload

---
+++ vagrant-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Install Vagrant 
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install vagrant
+
+# Install KVM
+sudo apt install qemu qemu-kvm libvirt-clients libvirt-daemon-system virtinst bridge-utils
+kvm-ok
+
+# Install libvirt
+sudo apt install qemu libvirt-daemon-system libvirt-clients libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev ruby-libvirt ebtables dnsmasq-base
+sudo apt-get install build-essential
+vagrant plugin install vagrant-libvirt
+vagrant plugin install vagrant-mutate
